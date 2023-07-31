af230be7615e68aaae60e7c2681b8cfe2c867b20
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-07-31 14:49:15 +0100

    Project set up

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <p>
+          Weather Beaten App.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+
