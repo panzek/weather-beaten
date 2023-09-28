b7817ced50a9ba960b8ba8b3d96022b6b35d7b24
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-09-28 17:25:57 +0100

    Format API response fields to display data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,22 +3,26 @@ import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-//Create a new Date object
-const today = new Date();
-const currentDate = today.toLocaleDateString();
-const currentTime = today.toLocaleTimeString();
-
-
-const reset = () =>{
-  window.location.reload()
-}
-
-const {REACT_APP_API_URL, REACT_APP_API_KEY} = process.env;
-
 const Weather = () => {
+
+  //Create a new Date object
+  const today = new Date();
+   //Get current date
+  const currentDate = today.toLocaleDateString();
+  console.log(currentDate);
+  //Get current time
+  const currentTime = today.toLocaleTimeString();
+  console.log(currentTime);
+
+  const refresh = () =>{
+    window.location.reload()
+  }
+
 
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("");
+
+  const {REACT_APP_API_URL, REACT_APP_API_KEY} = process.env;
 
   useEffect(() => {
     const getWeather = async () => {
@@ -28,7 +32,7 @@ const Weather = () => {
       setWeather(data);
     }
     getWeather();
-  }, [city]);
+  }, [REACT_APP_API_URL, REACT_APP_API_KEY, city]);
 
   const handleChange = (e) => {
     setCity(e.target.value);
@@ -41,28 +45,38 @@ const Weather = () => {
   return (
     <>
       <Row>
-        <h3>Weather Beaten App</h3>
+        <h3>Weather Beaten</h3>
       </Row>
       <Row>
-        <button onClick={reset}>Reset</button>
+        <Col><input value={city} onChange={handleChange} placeholder="Enter City..." /><button onClick={refresh}>Refresh</button></Col>
       </Row>
       <Row>
-      <Col><input value={city} onChange={handleChange} placeholder="Enter City..." /><button onClick={handleClick}>Search</button></Col>
+        <Col><button onClick={handleClick}>Search</button></Col>
       </Row>
-      <Row>
-        <Col>{city}</Col>
-      </Row>
-      <Row>
-        Date: {currentDate} Time: {currentTime}
-      </Row>
-      <Row>
-        <Col sm={6}>Temp: 1oC</Col>
-        <Col sm={6}>Wind speed: 2.68km/h</Col>
-      </Row>
-      <Row>
-        <Col sm={6}>Humidity: 90%</Col>
-        <Col sm={6}>Wind speed: 2.68km/h</Col>
-      </Row>
+      {(typeof weather.main !== "undefined") ? (
+        <Row>
+          <Row>
+            <Col>{weather.name}</Col>
+          </Row>
+          <Row>
+           <Col>{currentDate} {currentTime}</Col>
+          </Row>
+          <Row>
+            <Col>Icon: {weather.weather.icon} </Col>
+            <Col>Timezone: {weather.timezone} </Col>
+          </Row>
+          <Row>
+          <Col sm={6}>Humidity: {weather.main.humidity}%</Col>
+          <Col sm={6}>Temperature: {Math.floor(weather?.main?.temp)}&deg;</Col>
+          </Row>
+          <Row>
+            <Col sm={6}>Description: {weather.weather[0].description} </Col>
+            <Col sm={6}>Description: {weather.weather[0].icon} </Col>
+            <Col sm={6}>Wind speed: {weather.wind.speed}km/hr </Col>
+          </Row>
+        </Row>
+      ) : <Row>Enter city and click search button</Row>
+      }
     </>
    
   )
