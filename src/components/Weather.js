a5578c80e4485ca178a3033505ed9e6aad0e137c
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-10-12 15:04:12 +0100

    Remove effect to handle user events

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 // import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,41 +9,59 @@ const Weather = () => {
   const today = new Date();
    //Get current date
   const currentDate = today.toLocaleDateString();
-  console.log(currentDate);
+  // console.log(currentDate);
   //Get current time
   const currentTime = today.toLocaleTimeString();
-  console.log(currentTime);
+  // console.log(currentTime);
 
   const refresh = () =>{
     window.location.reload()
   }
-
 
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("");
 
   const {REACT_APP_API_URL, REACT_APP_API_KEY} = process.env;
 
+  //1. Fetching data from weather API Without using useEffect 
+  const getWeather = async () => {
+    const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
+    const data = await response.json();
+    setWeather(data);
+  };
+
+  //2. a. Fetching data from weather API using useEffect without useCallback function
+  //b. fix "race condition" with cleanup function to ignore stale responses
   // useEffect(() => {
   //   const getWeather = async () => {
+  //     let ignore = false;
   //     const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
   //     const data = await response.json();
-  //     console.log(data);
-  //     setWeather(data);
-  //   }
+  //      if (!ignore) {
+  //       setWeather(data);
+  //      }
+  //      return () => {
+  //       ignore = true;
+  //     };
+  //   };
   //   getWeather();
   // }, [REACT_APP_API_URL, REACT_APP_API_KEY, city]);
 
-  const getWeather = useCallback(async () => {
-    const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
-    const data = await response.json();
-    console.log(data);
-    setWeather(data);
-  }, [REACT_APP_API_URL, city, REACT_APP_API_KEY]);
+  // const handleClick = () => {
+  //   setWeather(weather);
+  // }
 
-  useEffect(() => {
-    getWeather();
-  }, [getWeather]);
+  // 3. Because 'getWeather' function makes the dependencies of useEffect Hook change on every render. 
+  //To fix this, we wrap the definition of 'getWeather' in its own useCallback() Hook. 
+  // const getWeather = useCallback(async () => {
+  //   const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
+  //   const data = await response.json();
+  //   setWeather(data);
+  // }, [REACT_APP_API_URL, city, REACT_APP_API_KEY]);
+
+  // useEffect(() => {
+  //   getWeather();
+  // }, [getWeather]);
 
   const handleChange = (e) => {
     setCity(e.target.value);
@@ -59,7 +77,7 @@ const Weather = () => {
     if(!weather) return null;
 
     const icon = weather.weather[0].icon;
-    console.log(icon)
+    // console.log(icon)
     const getIconUrl = `https://openweathermap.org/img/wn/${icon.replace('n','d')}@2x.png`;
 
     return <img src={getIconUrl} width="250" height="250" alt="Weather Icon"></img>
