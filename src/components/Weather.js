d304f46080f99358532759639d8c7c0e1a22baa0
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-10-14 14:37:08 +0100

    Conditionally disable input field and submit button

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,10 +9,8 @@ const Weather = () => {
   const today = new Date();
    //Get current date
   const currentDate = today.toLocaleDateString();
-  // console.log(currentDate);
   //Get current time
   const currentTime = today.toLocaleTimeString();
-  // console.log(currentTime);
 
   const refresh = () =>{
     window.location.reload()
@@ -20,11 +18,12 @@ const Weather = () => {
 
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("");
+  const [status, setStatus] = useState("typing");
   const [error, setError] = useState(false);
 
   const {REACT_APP_API_URL, REACT_APP_API_KEY} = process.env;
 
-  //1. Fetching data from weather API Without using useEffect 
+  // Fetching data from weather API without using useEffect 
   const getWeather = async () => {
     try {
       const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
@@ -39,45 +38,13 @@ const Weather = () => {
     }
   };
 
-  //2. a. Fetching data from weather API using useEffect without useCallback function
-  //b. fix "race condition" with cleanup function to ignore stale responses
-  // useEffect(() => {
-  //   const getWeather = async () => {
-  //     let ignore = false;
-  //     const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
-  //     const data = await response.json();
-  //      if (!ignore) {
-  //       setWeather(data);
-  //      }
-  //      return () => {
-  //       ignore = true;
-  //     };
-  //   };
-  //   getWeather();
-  // }, [REACT_APP_API_URL, REACT_APP_API_KEY, city]);
-
-  // const handleClick = () => {
-  //   setWeather(weather);
-  // }
-
-  // 3. Because 'getWeather' function makes the dependencies of useEffect Hook change on every render. 
-  //To fix this, we wrap the definition of 'getWeather' in its own useCallback() Hook. 
-  // const getWeather = useCallback(async () => {
-  //   const response = await fetch(`${REACT_APP_API_URL}/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
-  //   const data = await response.json();
-  //   setWeather(data);
-  // }, [REACT_APP_API_URL, city, REACT_APP_API_KEY]);
-
-  // useEffect(() => {
-  //   getWeather();
-  // }, [getWeather]);
-
   const handleChange = (e) => {
     setCity(e.target.value);
   }
 
   const handleClick = () => {
     setWeather(weather);
+    setStatus("submitting")
     getWeather();
   }
 
@@ -86,7 +53,6 @@ const Weather = () => {
     if(!weather) return null;
 
     const icon = weather.weather[0].icon;
-    // console.log(icon)
     const getIconUrl = `https://openweathermap.org/img/wn/${icon.replace('n','d')}@2x.png`;
 
     return <img src={getIconUrl} width="250" height="250" alt="Weather Icon"></img>
@@ -98,10 +64,23 @@ const Weather = () => {
         <h3>Weather Beaten</h3>
       </Row>
       <Row>
-        <Col><input value={city} onChange={handleChange} placeholder="Enter City..." /><button onClick={refresh}>Refresh</button></Col>
+        <Col>
+          <input 
+            value={city} 
+            onChange={handleChange} 
+            disabled={status === "submitting"} 
+            placeholder="Enter City..." />
+          <button onClick={refresh}>Refresh</button>
+        </Col>
       </Row>
       <Row>
-        <Col><button onClick={handleClick}>Check Weather</button></Col>
+        <Col>
+        <button 
+          onClick={handleClick} 
+          disabled={city.length === 0 || status === "submitting"} >
+          Check Weather
+        </button>
+        </Col>
       </Row>
       <Row>
         <Col>{ error && <div>{error}</div> }</Col>
@@ -123,7 +102,7 @@ const Weather = () => {
           </Row>
           <Row>
           <Col sm={6}>Humidity: {weather.main.humidity}%</Col>
-          <Col sm={6}>Temperature: {Math.floor(weather.main.temp)}&deg;</Col>
+          <Col sm={6}>Temperature: {Math.floor(weather.main.temp)}&deg;C</Col>
           </Row>
           <Row>
             <Col sm={6}>Wind speed: {weather.wind.speed}km/hr </Col>
