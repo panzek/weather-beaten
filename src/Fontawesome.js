2cc00e76c16fe90eaa54cf0fdeaba5e0f20a1cbe
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-11-05 14:11:28 +0000

    Create fontawesome.js and import icons via library

---
+++ src/Fontawesome.js
@@ -0,0 +1,15 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { 
+    fas,
+    faTwitter, 
+    faInstagram,
+    faGithub 
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    fas, 
+    faTwitter, 
+    faInstagram,
+    faGithub
+    );
