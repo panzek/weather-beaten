9f10ae359ade1ceaff1949077dee226a2e0cfb60
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-11-06 11:43:34 +0000

    Style social icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navbar.js';
 import Weather from './components/Weather';
 import './Fontawesome.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const App = () => {
   return (
@@ -15,22 +16,30 @@ const App = () => {
             <Weather />
           </header>
         </div>
-        <div className="col socials"> 
-          <span>
-            Social Network
-          </span>
-          <span>
-          <a href="https://twitter.com/ipanzek" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon="fa-brands fa-twitter" />
-          </a>
-          <a href="https://instagram.com/panzek" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon="fa-brands fa-instagram" />
-          </a>
-          <a href="https://github.com/panzek" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon="fa-brands fa-github" />
-          </a>
-          </span>
-        </div>
+        <Container className="col socials"> 
+          <Row>
+            <Col>
+              Social Network
+            </Col>
+          </Row>
+          <Row >
+            <Col>
+            <a href="https://twitter.com/ipanzek" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon="fa-brands fa-twitter" size="lg"  className="icon-color"/>
+            </a>
+            </Col>
+            <Col>
+              <a href="https://instagram.com/panzek" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon="fa-brands fa-instagram" size="lg" className="icon-color"/>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/panzek" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon="fa-brands fa-github" size="lg" className="icon-color"/>
+              </a>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   )
