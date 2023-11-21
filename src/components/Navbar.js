23a3e1ca27d1deb38f07ef134bc767195924e485
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-11-21 16:58:56 +0000

    Remove offcanvas component from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Nav, Navbar, Offcanvas} from 'react-bootstrap';
+import {Container, Navbar} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from '../images/weatherBeaten-logo.webp';
 
@@ -20,22 +20,6 @@ const Navigation = () => {
               WeatherBeaten
             </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  WeatherBeaten
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">About Me</Nav.Link>  
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
           </Container>
         </Navbar>
       ))}
