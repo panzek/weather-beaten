811fe273c5dbbc8a78d62c9e6e34c71b2c4b9746
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-11-21 16:49:28 +0000

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Container, Nav, Navbar, Offcanvas} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from '../images/weatherBeaten-logo.webp';
 
 const Navigation = () => {
   return (
@@ -8,7 +9,16 @@ const Navigation = () => {
       {['sm'].map((expand) => (
         <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
           <Container fluid className="navigation">
-            <Navbar.Brand href="#">WeatherBeaten</Navbar.Brand>
+            <Navbar.Brand href="#">
+            <img
+              alt="weatherBeaten-logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+              WeatherBeaten
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
